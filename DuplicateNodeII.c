05d2e4d1e1aee654cdc5dd63177d4c56f167aea7
Shaik Faizan Roshan Ali <67701441+faizanroshan@users.noreply.github.com>
2021-01-15 10:17:32 +0530

    Update DuplicateNodeII.c

--- DuplicateNodeII.c
+++ DuplicateNodeII.c
@@ -5,34 +5,34 @@ struct ListNode {
 
     int val;
     struct ListNode *next;
- }*head = NULL, *ptr = NULL;
+}*head = NULL, *ptr = NULL;
 
 struct ListNode *createLL(struct ListNode *head) {
-    
+
     int num;
     printf("Enter the number: ");
     scanf("%d", &num);
-    
+
     while(num != -1) {
-        
+
         struct ListNode *newNode = (struct ListNode *)malloc(sizeof(struct ListNode));
         newNode -> val = num;
         if(head == NULL) {
-            
+
             head = newNode;
         }
-        
+
         else {
-        
+
             ptr = head;
             while(ptr -> next = NULL) {
-                
+
                 ptr = ptr -> next;
             }
-            
+
             ptr -> next = newNode;
         }
-        
+
         newNode -> next = NULL;
         printf("Enter the number: ");
         scanf("%d", &num);
@@ -40,57 +40,57 @@ struct ListNode *createLL(struct ListNode *head) {
     return head;
 }
 struct ListNode* deleteDuplicates(struct ListNode* head){
-    
+
     static int freqMap[201];
-    
+
     while(ptr != NULL) {
-        
+
         freqMap[ptr -> val + 100]++;
         ptr = ptr -> next;
     }
-    
+
     free(ptr);
-    struct ListNode *currentPtr = head -> next;
-    struct ListNode *prevPtr = head;
-    
+    struct ListNode *currentPtr = head;
+    struct ListNode *prevPtr = NULL;
+
     while(currentPtr != NULL) {
-        
-        if(prevPtr == head && freqMap[prevPtr -> val + 100] > 1) {
-            
+
+        if(currentPtr == head && freqMap[prevPtr -> val + 100] > 1) {
+
             ptr = head;
-            head = head -> next;
-            prevPtr = head;
             currentPtr = currentPtr -> next;
+            head = currentPtr;
+
             free(ptr);
         }
-        
-        else if ( freqMap[currentPtr -> val + 100] > 1){
-            
+
+        else if ( freqMap[currentPtr -> val + 100] > 1 && head != currentPtr){
+
             prevPtr -> next = currentPtr -> next;
             ptr = currentPtr;
             currentPtr = currentPtr -> next;
             free(ptr);
         }
-        
+
         else {
-            
-            prevPtr = prevPtr -> next;
+
+            prevPtr = currentPtr;
             currentPtr = currentPtr -> next;
         }
     }
-    
+
     ptr = head;
     while(ptr != NULL) {
-        
+
         printf("%d ", ptr -> val);
         ptr = ptr -> next;
     }
-    
+
     return head;
 }
 
 int main() {
-    
+
     head = createLL(head);
     head = deleteDuplicates(head);
 }
