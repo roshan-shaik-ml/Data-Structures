5f5755ddcb429346257c1cdb70cf94dcbadaba13
Shaik Faizan Roshan Ali <67701441+faizanroshan@users.noreply.github.com>
2021-01-17 21:09:06 +0530

    Update middleOfLinkedList.c

--- middleOfLinkedList.c
+++ middleOfLinkedList.c
@@ -1,3 +1,16 @@
+/*
+ *    Author: Shaik Faizan Roshan Ali
+ *    Date: 17th January 2020
+ *    Email: alsahercoder@gmail.com
+ *    Description: Return the middle node of a Linked List.
+ *    Source: https://leetcode.com/problems/middle-of-the-linked-list/
+ *    Approach:
+ *          - Count the totalNodes
+ *          - Traverse a pointer to middle node of the linked list. 
+ *          - In case the number is even, return the second node of the middle two nodes.
+ *          - else, return the middle nodes.
+ *
+ */
 struct ListNode* middleNode(struct ListNode* head){
     
     struct ListNode *ptr = head;
