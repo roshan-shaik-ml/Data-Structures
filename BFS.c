83353f0c5a0278ae53aa70a59cd26c06dac05ebe
Shaik Faizan Roshan Ali <67701441+faizanroshan@users.noreply.github.com>
2021-01-27 19:32:20 +0530

    Update BFS.c

--- BFS.c
+++ BFS.c
@@ -1,65 +1,66 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 
 struct node {
-	
-	int data;
-	struct node *right;
-	struct node *left;
+    
+    int data;
+    struct node *right;
+    struct node *left;
 }*root = NULL;
 
 struct QNode {
-	
-	int data;
-	struct node *treeNode;
-	struct QNode *next;
+    
+    int data;
+    struct node *treeNode;
+    struct QNode *next;
 }*head = NULL, *tail = NULL;
 
 void enqueue(int num, struct node *Node) {
-	
-	/* insert in the end */
-	struct QNode *enqueueNode = (struct QNode*)malloc(sizeof(struct QNode));
-	enqueueNode -> treeNode = Node;
-	enqueueNode -> data = num;
-	enqueueNode -> next = NULL;
-	if(tail == NULL && head == NULL) {
-		
-		head = enqueueNode;
-		tail = enqueueNode;
-		return;
-	}
-	
-	else {
-		
-		tail -> next = enqueueNode;
-		tail = tail -> next;	
-	}
+    
+    /* insert in the end */
+    struct QNode *enqueueNode = (struct QNode*)malloc(sizeof(struct QNode));
+    enqueueNode -> treeNode = Node;
+    enqueueNode -> data = num;
+    enqueueNode -> next = NULL;
+    if(tail == NULL && head == NULL) {
+        
+        head = enqueueNode;
+        tail = enqueueNode;
+        return;
+    }
+    
+    else {
+        
+        tail -> next = enqueueNode;
+        tail = tail -> next;    
+    }
 }
 
 void dequeue(struct QNode *head) {
-	
-	/* remove the beginning node */
-	struct QNode *ptr = head;
-	head = ptr -> next;
-	free(ptr);	
+    
+    /* remove the beginning node */
+    struct QNode *ptr = head;
+    head = ptr -> next;
+    free(ptr);    
     /* free the pointer */
 }
 
 void printLevelOrder(struct node *root) {
-	
-	int flag = 0;
-	struct QNode *parentNode = root;
-	
-	while(head != NULL && flag != 1) {
-		
-		flag = 1; /* To avoid the intial state of queue */
-		enqueue(ptr -> data, ptr); 
-		enqueue(ptr -> right -> data, ptr -> right);
-		enqueue(ptr -> left -> data, ptr -> left);
-		dequeue(head);
-	}
+    
+    int flag = 0;
+    struct QNode *parentNode = root;
+    
+    while(head != NULL && flag != 1) {
+        
+        flag = 1; /* To avoid the intial state of queue */
+        enqueue(ptr -> data, ptr); 
+        enqueue(ptr -> right -> data, ptr -> right);
+        enqueue(ptr -> left -> data, ptr -> left);
+        dequeue(head);
+    }
 }
 int main() {
-	
-	return 0;
+    
+    return 0;
 }
